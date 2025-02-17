8f4aeff6801eba16c57b2d7125d378aff7390678
Niladri Sekhar Rout <niladri30012003@gmail.com>
2025-02-17 22:11:25 +0530

    push

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,10 +1,21 @@
 // backend/routes/userRoutes.js
 const express = require('express');
-const {registerUser,loginUser} =require ("../controller/userController");
+const {registerUser,loginUser,getProfile,updateProfile} =require ("../controller/userController");
+const multer = require('multer');
 const protect = require ("../middleware/authMiddleware");
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
+
 const router = express.Router();
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
+
+
+// Protected Route for Profile Get and Update (using 'protect' middleware)
+router.get('/profile', getProfile);
+router.put('/profile', upload.single('profilePicture'), updateProfile);
 
 module.exports = router;
